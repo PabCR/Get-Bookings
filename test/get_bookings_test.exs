d5f36d89f8f623cb139d6ee32411effa4fb096c6
PabCR <palv2602@gmail.com>
2024-06-06 18:54:34 -0400

    Format

--- test/get_bookings_test.exs
+++ test/get_bookings_test.exs
@@ -4,19 +4,27 @@ defmodule GetBookingsTest do
 
   describe "fetch_data/1" do
     test "fetches data from valid url" do
-      case GetBookings.fetch_data("https://dz4o5.wiremockapi.cloud/productmgmt/api/api/searchbooking") do
+      case GetBookings.fetch_data(
+             "https://dz4o5.wiremockapi.cloud/productmgmt/api/api/searchbooking"
+           ) do
         {:ok, _} -> :ok
         _ -> assert false
       end
     end
+
     test "returns not_found from invalid url" do
-      case GetBookings.fetch_data("https://dz4o5.wiremockapi.cloud//productmgmt/api/api/searchbooking") do
+      case GetBookings.fetch_data(
+             "https://dz4o5.wiremockapi.cloud//productmgmt/api/api/searchbooking"
+           ) do
         {:not_found, _} -> :ok
         _ -> assert false
       end
     end
+
     test "returns error from invalid url" do
-      case GetBookings.fetch_data("https://dz4o5.wiremockapis.cloud/productmgmt/api/api/searchbooking") do
+      case GetBookings.fetch_data(
+             "https://dz4o5.wiremockapis.cloud/productmgmt/api/api/searchbooking"
+           ) do
         {:error, _} -> :ok
         _ -> assert false
       end
