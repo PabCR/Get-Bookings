607b03ba46879b4064646198e7c0014bd4328992
PabCR <palv2602@gmail.com>
2024-06-06 18:48:22 -0400

    Fetching from API and tests

--- lib/get_bookings.ex
+++ lib/get_bookings.ex
@@ -4,6 +4,15 @@ defmodule GetBookings do
   """
 
   def fetch_data(url) do
-
+    token = "9e3011c7-e012-4f42-94af-9ed6234d3dd6"
+    headers = [{"Authorization", "Bearer #{token}"}]
+    case HTTPoison.request(:post, url,"" , headers) do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+        {:ok, body}
+      {:ok, %HTTPoison.Response{status_code: 404}} ->
+        {:not_found, "Not found"}
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        {:error, reason}
+    end
   end
 end
--- test/get_bookings_test.exs
+++ test/get_bookings_test.exs
@@ -2,7 +2,24 @@ defmodule GetBookingsTest do
   use ExUnit.Case
   doctest GetBookings
 
-  test "greets the world" do
-    assert GetBookings.hello() == :world
+  describe "fetch_data/1" do
+    test "fetches data from valid url" do
+      case GetBookings.fetch_data("https://dz4o5.wiremockapi.cloud/productmgmt/api/api/searchbooking") do
+        {:ok, _} -> :ok
+        _ -> assert false
+      end
+    end
+    test "returns not_found from invalid url" do
+      case GetBookings.fetch_data("https://dz4o5.wiremockapi.cloud//productmgmt/api/api/searchbooking") do
+        {:not_found, _} -> :ok
+        _ -> assert false
+      end
+    end
+    test "returns error from invalid url" do
+      case GetBookings.fetch_data("https://dz4o5.wiremockapis.cloud/productmgmt/api/api/searchbooking") do
+        {:error, _} -> :ok
+        _ -> assert false
+      end
+    end
   end
 end
