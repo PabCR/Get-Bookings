cf7998dcad0b4edeaf0bc78675d65d32efad45a0
PabCR <palv2602@gmail.com>
2024-06-06 18:14:54 -0400

    Initial Commit

---
+++ lib/get_bookings.ex
@@ -0,0 +1,9 @@
+defmodule GetBookings do
+  @moduledoc """
+  Documentation for `GetBookings`.
+  """
+
+  def fetch_data(url) do
+
+  end
+end
---
+++ test/get_bookings_test.exs
@@ -0,0 +1,8 @@
+defmodule GetBookingsTest do
+  use ExUnit.Case
+  doctest GetBookings
+
+  test "greets the world" do
+    assert GetBookings.hello() == :world
+  end
+end
