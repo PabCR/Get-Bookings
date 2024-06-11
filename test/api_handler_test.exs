7b7e97efc53b3822c6fee8f0b1bdde823927f642
PabCR <palv2602@gmail.com>
2024-06-10 21:43:18 -0400

    Testing with vcr tests

--- test/api_handler_test.exs
+++ test/api_handler_test.exs
@@ -1,32 +1,40 @@
-defmodule GetBookingsTest do
+defmodule APIHandlerTest do
   use ExUnit.Case
-  doctest GetBookings
+  use ExVCR.Mock, adapter: ExVCR.Adapter.Hackney
 
-  describe "fetch_data/1" do
-    test "fetches data from valid url" do
-      case GetBookings.fetch_data(
-             "https://dz4o5.wiremockapi.cloud/productmgmt/api/api/searchbooking"
-           ) do
-        {:ok, _} -> :ok
-        _ -> assert false
+  doctest APIHandler
+
+  describe "get_bookings/1" do
+    test "returns {:ok, _} from valid url" do
+      use_cassette "get_bookings_valid_url" do
+        case APIHandler.get_bookings(
+               "/productmgmt/api/api/searchbooking"
+             ) do
+          {:ok, _} -> :ok
+          _ -> assert false
+        end
       end
     end
 
     test "returns not_found from invalid url" do
-      case GetBookings.fetch_data(
-             "https://dz4o5.wiremockapi.cloud//productmgmt/api/api/searchbooking"
-           ) do
-        {:not_found, _} -> :ok
-        _ -> assert false
+      use_cassette "get_bookings_invalid_url" do
+        case APIHandler.get_bookings(
+               "/axproductmgmt/api/api/searchbooking"
+             ) do
+          {:not_found, _} -> :ok
+          _ -> assert false
+        end
       end
     end
 
     test "returns error from invalid url" do
-      case GetBookings.fetch_data(
-             "https://dz4o5.wiremockapis.cloud/productmgmt/api/api/searchbooking"
-           ) do
-        {:error, _} -> :ok
-        _ -> assert false
+      use_cassette "get_bookings_error_url" do
+        case APIHandler.get_bookings(
+               "some_invalid_url"
+             ) do
+          {:error, _} -> :ok
+          _ -> assert false
+        end
       end
     end
   end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,4 @@
 ExUnit.start()
+
+# Configure ExVCR
+ExVCR.Config.cassette_library_dir("test/fixtures/vcr_cassettes")
