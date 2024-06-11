e7d2e1b7d8d9a4f7adda2b11f7889148b5134087
PabCR <palv2602@gmail.com>
2024-06-10 22:10:01 -0400

    Reformatting VCR Tests

--- test/api_handler_test.exs
+++ test/api_handler_test.exs
@@ -7,28 +7,19 @@ defmodule APIHandlerTest do
   describe "get_bookings/1" do
     test "returns {:ok, _} from valid url" do
       use_cassette "get_bookings_valid_url" do
-        case APIHandler.get_bookings("/productmgmt/api/api/searchbooking") do
-          {:ok, _} -> :ok
-          _ -> assert false
-        end
+        assert {:ok, _} = APIHandler.get_bookings("/productmgmt/api/api/searchbooking")
       end
     end
 
     test "returns not_found from invalid url" do
       use_cassette "get_bookings_invalid_url" do
-        case APIHandler.get_bookings("/axproductmgmt/api/api/searchbooking") do
-          {:not_found, _} -> :ok
-          _ -> assert false
-        end
+        assert {:not_found, _} = APIHandler.get_bookings("/axproductmgmt/api/api/searchbooking")
       end
     end
 
     test "returns error from invalid url" do
       use_cassette "get_bookings_error_url" do
-        case APIHandler.get_bookings("some_invalid_url") do
-          {:error, _} -> :ok
-          _ -> assert false
-        end
+        assert {:error, _} = APIHandler.get_bookings("some_invalid_url")
       end
     end
   end
