085a604aa9e1f5bff29a442cde4e8c2c16171d8b
PabCR <palv2602@gmail.com>
2024-06-10 21:44:49 -0400

    Testing ExVCR Reformatted

--- test/api_handler_test.exs
+++ test/api_handler_test.exs
@@ -7,9 +7,7 @@ defmodule APIHandlerTest do
   describe "get_bookings/1" do
     test "returns {:ok, _} from valid url" do
       use_cassette "get_bookings_valid_url" do
-        case APIHandler.get_bookings(
-               "/productmgmt/api/api/searchbooking"
-             ) do
+        case APIHandler.get_bookings("/productmgmt/api/api/searchbooking") do
           {:ok, _} -> :ok
           _ -> assert false
         end
@@ -18,9 +16,7 @@ defmodule APIHandlerTest do
 
     test "returns not_found from invalid url" do
       use_cassette "get_bookings_invalid_url" do
-        case APIHandler.get_bookings(
-               "/axproductmgmt/api/api/searchbooking"
-             ) do
+        case APIHandler.get_bookings("/axproductmgmt/api/api/searchbooking") do
           {:not_found, _} -> :ok
           _ -> assert false
         end
@@ -29,9 +25,7 @@ defmodule APIHandlerTest do
 
     test "returns error from invalid url" do
       use_cassette "get_bookings_error_url" do
-        case APIHandler.get_bookings(
-               "some_invalid_url"
-             ) do
+        case APIHandler.get_bookings("some_invalid_url") do
           {:error, _} -> :ok
           _ -> assert false
         end
